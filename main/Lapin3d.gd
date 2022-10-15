6470a51deb6fad69616b4ef2e70d1c7fc1a18bb1
ouaksel <ouakselboukhalfa.ob@gmail.com>
2022-10-15 14:26:58 +0200

    lapin mange food

--- main/Lapin3d.gd
+++ main/Lapin3d.gd
@@ -1,8 +1,9 @@
 extends RigidBody
 
-
+export(float, 1, 20) var speed = 5
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		apply_central_impulse(Vector3(-1,0,0))
@@ -13,3 +14,10 @@ func _process(delta):
 	if Input.is_action_pressed("ui_up"):
 		apply_central_impulse(Vector3(0,0,1))
 
+
+#func _integrate_forces(state):
+#	var input_h = Input.get_action_strength("ui_right") -  Input.get_action_strength("ui_left") 
+#	var input_v = Input.get_action_strength("ui_down") -  Input.get_action_strength("ui_up") 
+#
+#	var force = Vector3(input_h, 0, input_v)
+#	add_central_force(force.normalized()*speed)
---
+++ main/pick Up.gd
@@ -0,0 +1,8 @@
+extends Area
+
+
+
+
+
+func _on_Pick_Up_body_entered(body):
+	queue_free()
