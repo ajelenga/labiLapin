741ddae7d1591f0c8ace07265cd8f3cb761e1231
ouaksel <ouakselboukhalfa.ob@gmail.com>
2022-10-19 10:41:43 +0200

    lapin move

--- main/Lapin3d.gd
+++ main/Lapin3d.gd
@@ -1,6 +1,5 @@
 extends RigidBody
 
-export(float, 1, 20) var speed = 5
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
@@ -10,14 +9,12 @@ func _process(delta):
 	if Input.is_action_pressed("ui_left"):
 		rotate_y(-0.1)
 	if Input.is_action_pressed("ui_down"):
-		translate_object_local(Vector3(-1,0,0))
+		var angle : float = self.rotation.y 
+		var fx : float = cos(angle)
+		var fy : float = sin(angle)
+		apply_central_impulse(Vector3(fx,0,fy))
 	if Input.is_action_pressed("ui_up"):
-		translate_object_local(Vector3(1,0,0))
+		translate_object_local(Vector3(0.1,0,0))
 
+		
 
-#func _integrate_forces(state):
-#	var input_h = Input.get_action_strength("ui_right") -  Input.get_action_strength("ui_left") 
-#	var input_v = Input.get_action_strength("ui_down") -  Input.get_action_strength("ui_up") 
-#
-#	var force = Vector3(input_h, 0, input_v)
-#	add_central_force(force.normalized()*speed)
