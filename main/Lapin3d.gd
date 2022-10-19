b363f33f57589c0769b14ee204eb31a834a0245c
ouaksel <ouakselboukhalfa.ob@gmail.com>
2022-10-19 09:22:20 +0200

    move

--- main/Lapin3d.gd
+++ main/Lapin3d.gd
@@ -6,13 +6,13 @@ export(float, 1, 20) var speed = 5
 
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
-		apply_central_impulse(Vector3(-1,0,0))
+		rotate_y(0.1)
 	if Input.is_action_pressed("ui_left"):
-		apply_central_impulse(Vector3(1,0,0))
+		rotate_y(-0.1)
 	if Input.is_action_pressed("ui_down"):
-		apply_central_impulse(Vector3(0,0,-1))
+		translate_object_local(Vector3(-1,0,0))
 	if Input.is_action_pressed("ui_up"):
-		apply_central_impulse(Vector3(0,0,1))
+		translate_object_local(Vector3(1,0,0))
 
 
 #func _integrate_forces(state):
