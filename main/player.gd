62e273704dc096c36849b22d39886dd2181b45fe
ouaksel <ouakselboukhalfa.ob@gmail.com>
2022-11-12 13:47:03 +0100

    move

---
+++ main/player.gd
@@ -0,0 +1,63 @@
+extends KinematicBody
+
+
+var direction = Vector3.BACK
+var velocity = Vector3.ZERO
+var strafe_dir = Vector3.ZERO
+var strafe = Vector3.ZERO
+
+var aim_turn = 0
+
+var vertical_velocity = 0
+var gravity = 20
+
+var movement_speed = 0
+var walk_speed = 5
+var run_speed = 5
+var acceleration = 6
+var angular_acceleration = 7
+
+var roll_magnitude = 17
+ 
+func _input(event):
+	if event is InputEventKey:
+		if event.as_text() == "Z" || event.as_text() == "Q" || event.as_text() == "S" || event.as_text() == "D" || event.as_text() == "Space":
+			if event.pressed:
+				get_node("Status/" + event.as_text()).color = Color("ff6666")
+			else:
+				get_node("Status/" + event.as_text()).color = Color("ffffff")
+
+
+func _physics_process(delta):
+	
+	
+
+	if Input.is_action_pressed("forward") ||  Input.is_action_pressed("backward") ||  Input.is_action_pressed("left") ||  Input.is_action_pressed("right"):
+		var h_rot = $Camroot/h.global_transform.basis.get_euler().y
+	
+		direction = Vector3(Input.get_action_strength("left") - Input.get_action_strength("right"),
+					0,
+					Input.get_action_strength("forward") - Input.get_action_strength("backward")).rotated(Vector3.UP,h_rot).normalized()
+		if Input.is_action_pressed("sprint"):
+					movement_speed = run_speed
+		else:
+			movement_speed = walk_speed
+	else:
+		movement_speed = 0
+	
+	velocity = lerp(velocity, direction * movement_speed, delta * acceleration)
+	move_and_slide(velocity + Vector3.DOWN * vertical_velocity, Vector3.UP)
+	
+	if !is_on_floor():
+		vertical_velocity += gravity * delta
+	else:
+		vertical_velocity = 0
+	
+	$Mesh.rotation.y = lerp_angle($Mesh.rotation.y, atan2(direction.x,direction.z), delta * angular_acceleration)
+	
+	$Status/Label.text = "direction : " + String(direction)
+	$Status/Label2.text = "direction.length() : " + String(direction.length())
+	$Status/Label3.text = "velocity : " + String(velocity)
+	$Status/Label4.text = "velocity.length() : " + String(velocity.length())
+
+	
